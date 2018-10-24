832a05a5fe000e941419b7036291ab6aadb6cb60
Aron Shamash <aronshamash@gmail.com>
2018-10-24 20:15:29 +0100

    feat: styling header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,10 +1,62 @@
+import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
 
+const Logo = styled.h1`
+  padding: 2rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+
+  a {
+    text-decoration: none;
+    background: ${props => props.theme.red};
+    color: ${props => props.theme.white};
+    padding: 1rem;
+  }
+
+  @media (max-width: 1200px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+`;
+
 const Header = () => (
-    <div>
-      <p>Welcome</p>
+  <StyledHeader>
+    <div className="bar">
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
+    <div className="sub-bar">
+      search
+    </div>
+    <div>
+      Cart
+    </div>
+  </StyledHeader>
 );
 
 export default Header;
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,20 +1,24 @@
 import Link from 'next/link';
+import StyledNav from './styles/NavStyles';
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>    
-        <Link prefetch href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>    
-        <Link prefetch href="/sell">
-          <a>Sell</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
+  <StyledNav>
+    <Link prefetch href="/items">
+      <a>Items</a>
+    </Link>
+    <Link prefetch href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link prefetch href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link prefetch href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link prefetch href="/me">
+      <a>Me</a>
+    </Link>
+  </StyledNav>
 );
 
 export default Nav;
